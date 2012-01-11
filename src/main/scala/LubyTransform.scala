23d99a5457961599b01b2570fe8c83ae3da0efff
Craig Blake <craigwblake@gmail.com>
2012-01-11 09:52:29 -0500

    Working Luby Transformation for arrays of all sizes.

--- src/main/scala/LubyTransform.scala
+++ src/main/scala/LubyTransform.scala
@@ -1,6 +1,7 @@
 package algorithms
 
 import scala.annotation.tailrec
+import scala.collection.immutable._
 import scala.collection.immutable.Stream._
 import scala.util.Random
 
@@ -11,55 +12,81 @@ import scala.util.Random
  *
  * 1. Choose a psuedo-random number, d, between 1 and k (number of total blocks in file/stream)
  * 2. Choose d blocks psuedo-randomly (seeded with d) from the file and combine them using xor
- * 3. Transmit encoded blocks aling with seed for the psuedo-random number
+ * 3. Transmit encoded blocks along with seed for the psuedo-random number
  *
- * Note that d should follow the Robust Soliton Distribution in order to optimize transmission size
+ * Note that d should follow the Robust Soliton Distribution for optimal transmission size
  */
-class LubyTransform (val seed: Int = Random.nextInt()) {
+class LubyTransform (val seed: Int = Random.nextInt(), val blockSize: Int = 32768) {
     import LubyTransform._
 
-    val random = new Random(seed)
+    /**
+     * Transforms a stream of data into a stream of encoded blocks using an RSD distribution function.
+     */
+    def transform (data: Seq[Array[Byte]]): Stream[Block] = transform(data, distribution(seed, data.length))
 
     /**
-     * Stream of random number used as a distribution for encoding.
+     * Transforms a stream of data into a stream of encoded blocks using the provided distribution function.
      */
-    val distribution: Stream[Int] = cons(random.nextInt(5) + 1, distribution)
-
-    /**
-     * Transforms a stream of input data into a stream of encoded blocks using the seeded distribution function.
-     */
-    def transform (data: Stream[Array[Byte]]): Stream[Block] = transformWithDistribution(data, distribution)
-
-    /**
-     * Transforms a stream of input data into a stream of encoded blocks using the provided distribution function.
-     */
-    private def transformWithDistribution (input: Stream[Array[Byte]], ints: Stream[Int]): Stream[Block] = input match {
+    private def transform (input: Seq[Array[Byte]], degrees: Stream[Int]): Stream[Block] = input match {
         case Empty => Empty
         case _ =>
-            val (xor, remainder) = combine(input.head, input.tail, ints.head)
-            cons(Block(seed, xor), transformWithDistribution(remainder, ints.tail))
+            val blocks = select(input, degrees.head, distribution(degrees.head, input.length - 1))
+            combine(blocks) match {
+                case None => Empty
+                case Some(xor) => cons(Block(degrees.head, false, xor), transform(input, degrees.tail))
+            }
     }
 }
 
 object LubyTransform {
 
     /**
-     * Combines an arbitrary number of equal length byte arrays by XOR
+     * Stream of random numbers from 0 (inclusive) to bound (exclusive), used as a distribution.
      */
-    @tailrec
-    def combine (head: Array[Byte], tail: Stream[Array[Byte]], remaining: Int): (Array[Byte], Stream[Array[Byte]]) = remaining match {
-        case 0 => (head, tail)
-        case _ if tail == Empty => (head, tail)
-        case _ => combine(xor(head, tail.head), tail.tail, remaining - 1)
+    def distribution (seed: Int, bound: Int): Stream[Int] = {
+        val random = new Random(seed)
+        def distribution (random: Random): Stream[Int] = cons(random.nextInt(bound + 1), distribution(random))
+        distribution(random)
     }
 
     /**
-     * Combines two byte arrays by XOR
+     * Nondestructively combines an arbitrary number of equal length byte arrays by XOR
+     */
+    def combine (data: Seq[Array[Byte]]): Option[Array[Byte]] = data match {
+        case Nil => None
+        case x::xs => combine(xs) match {
+            case None => Some(x)
+            case Some(x2) => Some(xor(x, x2))
+        }
+    }
+
+    /**
+     * Selects a unique psuedo-random set of blocks from the input data. Chooses via random index pulled from a stream
+     * and discards duplicates; for this reason the count must not be higher than the range of the random number
+     * generator.
+     */
+    def select (input: Seq[Array[Byte]], count: Int, random: Stream[Int], used: Set[Int] = Set()): List[Array[Byte]] = count match {
+        case 0 => Nil
+        case _ =>
+            val next = random.filter(!used.contains(_)).head
+            input(next) :: select(input, count - 1, random.tail, used + next)
+    }
+
+    /**
+     * Combines two byte arrays by XOR nondestructively
      */
     def xor (one: Array[Byte], two: Array[Byte]) = {
-        for (i <- 0 until two.length) one.update(i, (one(i) ^ two(i)).asInstanceOf[Byte])
-        one
+        val (larger, smaller) = sort(one, two)
+
+        val result = larger.clone
+        for (i <- 0 until smaller.length) result.update(i, (larger(i) ^ smaller(i)).asInstanceOf[Byte])
+        result
     }
+
+    /**
+     * Sort the arrays by size
+     */
+    def sort (one: Array[Byte], two: Array[Byte]) = if (one.length >= two.length) (one, two) else (two, one)
 }
 
-case class Block (val seed: Int, val data: Array[Byte])
+case class Block (val seed: Int, val last: Boolean, val data: Array[Byte])
--- src/test/scala/LubyTransformTest.scala
+++ src/test/scala/LubyTransformTest.scala
@@ -3,15 +3,29 @@ package algorithms
 import com.github.tototoshi.base64.Base64.encode
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import scala.annotation.tailrec
 import scala.collection.immutable.Stream._
 
 class LubyTransformSpec extends FlatSpec with ShouldMatchers {
 
-	"A Luby transform" should "XOR two byte arrays into a block" in {
-        val one = "abcd".getBytes("ASCII")
-        val two = "efgh".getBytes("ASCII")
+    implicit def string2bytes (v: String) = v.getBytes("ASCII")
 
-        val xor = LubyTransform.xor(one, two)
+    "A distribution" should "return pseudo-random integers between 1 and bound inclusively" in {
+        val stream = LubyTransform.distribution(1, 5)
+        stream.head should equal (3)
+        stream.tail.head should equal (4)
+        stream.tail.tail.head should equal (1)
+
+        @tailrec
+        def testBounds (stream: Stream[Int], remaining: Int = 1000): Unit = {
+            stream.head should be >= (0)
+            stream.head should be < (5)
+            testBounds(stream.tail, remaining - 1)
+        }
+    }
+
+	"A Luby Transform" should "XOR two byte arrays into a block" in {
+        val xor = LubyTransform.xor("abcd", "efgh")
 
         xor(0) ^ 'a' should equal ('e')
         xor(1) ^ 'b' should equal ('f')
@@ -24,43 +38,118 @@ class LubyTransformSpec extends FlatSpec with ShouldMatchers {
         xor(3) ^ 'h' should equal ('d')
 	}
 
+	it should "XOR two disparately sized byte arrays into a block" in {
+        val one = LubyTransform.xor("abcd", "efg")
+
+        one.length should equal (4)
+
+        one(0) ^ 'a' should equal ('e')
+        one(1) ^ 'b' should equal ('f')
+        one(2) ^ 'c' should equal ('g')
+        one(3) should equal ('d')
+
+        one(0) ^ 'e' should equal ('a')
+        one(1) ^ 'f' should equal ('b')
+        one(2) ^ 'g' should equal ('c')
+
+        val two = LubyTransform.xor("abc", "efgh")
+
+        two.length should equal (4)
+
+        two(0) ^ 'a' should equal ('e')
+        two(1) ^ 'b' should equal ('f')
+        two(2) ^ 'c' should equal ('g')
+        two(3) should equal ('h')
+
+        two(0) ^ 'e' should equal ('a')
+        two(1) ^ 'f' should equal ('b')
+        two(2) ^ 'g' should equal ('c')
+	}
+
     it should "XOR an arbitrary number of byte arrays into a block" in {
-        val one = "abcd".getBytes("ASCII")
-        val two = "efgh".getBytes("ASCII")
-        val three = "ijkl".getBytes("ASCII")
+        val one = "abcd"
+        val two = "efgh"
+        val three = "ijkl"
 
-        val xor = LubyTransform.combine(one, Stream(two, three), 2)
+        val xor = LubyTransform.combine(List[Array[Byte]](one, two, three)).get
 
-        xor._2 should equal (Empty)
+        xor.length should equal (4)
 
-        xor._1(0) ^ 'a' ^ 'e' should equal ('i')
-        xor._1(1) ^ 'b' ^ 'f' should equal ('j')
-        xor._1(2) ^ 'c' ^ 'g' should equal ('k')
-        xor._1(3) ^ 'd' ^ 'h' should equal ('l')
+        xor(0) ^ 'a' ^ 'e' should equal ('i')
+        xor(1) ^ 'b' ^ 'f' should equal ('j')
+        xor(2) ^ 'c' ^ 'g' should equal ('k')
+        xor(3) ^ 'd' ^ 'h' should equal ('l')
 
-        xor._1(0) ^ 'a' ^ 'i' should equal ('e')
-        xor._1(1) ^ 'b' ^ 'j' should equal ('f')
-        xor._1(2) ^ 'c' ^ 'k' should equal ('g')
-        xor._1(3) ^ 'd' ^ 'l' should equal ('h')
+        xor(0) ^ 'a' ^ 'i' should equal ('e')
+        xor(1) ^ 'b' ^ 'j' should equal ('f')
+        xor(2) ^ 'c' ^ 'k' should equal ('g')
+        xor(3) ^ 'd' ^ 'l' should equal ('h')
+    }
+
+    it should "XOR an arbitrary number of disparately sized byte arrays into a block" in {
+        val one = "abcd"
+        val two = "efgh"
+        val three = "ijk"
+
+        val xor = LubyTransform.combine(List[Array[Byte]](one, two, three)).get
+
+        xor.length should equal (4)
+
+        xor(0) ^ 'a' ^ 'e' should equal ('i')
+        xor(1) ^ 'b' ^ 'f' should equal ('j')
+        xor(2) ^ 'c' ^ 'g' should equal ('k')
+        xor(3) ^ 'd' should equal ('h')
+
+        xor(0) ^ 'a' ^ 'i' should equal ('e')
+        xor(1) ^ 'b' ^ 'j' should equal ('f')
+        xor(2) ^ 'c' ^ 'k' should equal ('g')
+        xor(3) ^ 'h' should equal ('d')
     }
 
     it should "consume a stream of byte arrays and create a stream of blocks" in {
-        val one = "abcd".getBytes("ASCII")
-        val two = "efgh".getBytes("ASCII")
-        val three = "ijkl".getBytes("ASCII")
-        val four = "mnop".getBytes("ASCII")
+        val one = "abcd"
+        val two = "efgh"
+        val three = "ijkl"
+        val four = "mnop"
 
-        val input = cons(one, cons(two, cons(three, Stream(four))))
+        val input = List[Array[Byte]](one, two, three, four)
 
-        // Magic number which seeds the test to encode two blocks
-        val fountain = new LubyTransform(1)
+        val fountain = new LubyTransform(2)
         val output: Stream[Block] = fountain.transform(input)
         
         output should not equal (Empty)
-        output.tail.tail should equal (Empty)
 
         val first = output.head
         val second = output.tail.head
+
+        first.data.length should equal (4)
+        first.data(0) ^ 'i' ^ 'a' should equal ('m')
+        first.data(1) ^ 'j' ^ 'b' should equal ('n')
+        first.data(2) ^ 'k' ^ 'c' should equal ('o')
+        first.data(3) ^ 'l' ^ 'd' should equal ('p')
+
+        second.data.length should equal (4)
+        second.data(0) ^ 'i' should equal ('e')
+        second.data(1) ^ 'j' should equal ('f')
+        second.data(2) ^ 'k' should equal ('g')
+        second.data(3) ^ 'l' should equal ('h')
+    }
+
+    it should "consume a stream of disparately sized byte arrays and create a stream of blocks" in {
+        val one = "abcd"
+        val two = "efgh"
+        val three = "ijk"
+
+        val input = List[Array[Byte]](one, two, three)
+
+        val fountain = new LubyTransform(2)
+        val output: Stream[Block] = fountain.transform(input)
+        
+        output should not equal (Empty)
+
+        val first = output.head
+        val second = output.tail.head
+        val third = output.tail.tail.head
 
         first.data.length should equal (4)
         first.data(0) ^ 'a' should equal ('e')
@@ -69,9 +158,15 @@ class LubyTransformSpec extends FlatSpec with ShouldMatchers {
         first.data(3) ^ 'd' should equal ('h')
 
         second.data.length should equal (4)
-        second.data(0) ^ 'i' should equal ('m')
-        second.data(1) ^ 'j' should equal ('n')
-        second.data(2) ^ 'k' should equal ('o')
-        second.data(3) ^ 'l' should equal ('p')
+        second.data(0) should equal ('a')
+        second.data(1) should equal ('b')
+        second.data(2) should equal ('c')
+        second.data(3) should equal ('d')
+
+        third.data.length should equal (4)
+        third.data(0) ^ 'i' ^ 'a' should equal ('e')
+        third.data(1) ^ 'j' ^ 'b' should equal ('f')
+        third.data(2) ^ 'k' ^ 'c' should equal ('g')
+        third.data(3) ^ 'd' should equal ('h')
     }
 }
